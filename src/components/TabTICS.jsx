9c49e292ae3d09666546cbced90f40b4815e5963
Audy2000 <153302584+Audy2000@users.noreply.github.com>
2024-03-19 12:47:22 -0500

    Update TabTICS.jsx

--- src/components/TabTICS.jsx
+++ src/components/TabTICS.jsx
@@ -14,22 +14,22 @@ function TabTICS() {
                     <Card
                         url_imagen="https://onedrive.live.com/embed?resid=FFE8717E7A93C957%21212&authkey=%21ACGzHPO2C5dfUHw&width=1366&height=768"
                         titulo="Proyectos WinForms"
-                        descripcion={"Proyecto Windows Forms para la gestion de Comprobantes Electrónicos"} />
+                        descripcion={"Proyecto Windows Forms para la gestión de Comprobantes Electrónicos"} />
 
                     <Card
                         url_imagen="https://onedrive.live.com/embed?resid=FFE8717E7A93C957%21210&authkey=%21ANmxrQnwYrhiMoI&width=660"
-                        titulo="Estensión de Google Chrome"
-                        descripcion={"Extensión de Google Chrome para la descarga de Documentos Electronicos"} />
+                        titulo="Extensión de Google Chrome"
+                        descripcion={"Extensión de Google Chrome para la descarga de Documentos Electrónicos"} />
 
                     <Card
                         url_imagen="https://www.ictacademy.in/pages/assets/base/img/content/stock/asp.jpg"
                         titulo="ASP"
-                        descripcion={"Creacion de APIs Rest con ASP.NET"} />
+                        descripcion={"Creación de APIs Rest con ASP.NET"} />
 
                     <Card
                         url_imagen="https://www.qindel.com/wp-content/uploads/2023/04/spring-boot.jpeg"
                         titulo="JAVA- Spring Boot"
-                        descripcion={"Creacion de APIs Rest con Java Spring Boot"} />
+                        descripcion={"Creación de APIs Rest con Java Spring Boot"} />
 
                     <Card
                         url_imagen="https://onedrive.live.com/embed?resid=FFE8717E7A93C957%21211&authkey=%21AE2pQFm32Jq48Io&width=660"
