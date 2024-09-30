ce971b261688597d6f7b1a060214f66f008cd009
Audy <aluvesistemas@gmail.com>
2024-09-30 10:29:34 -0500

    Se agregan nuevos items a la tab de TICS

--- src/components/TabTICS.jsx
+++ src/components/TabTICS.jsx
@@ -11,25 +11,27 @@ function TabTICS() {
 
                 <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
 
-                    <Card
-                        url_imagen="https://onedrive.live.com/embed?resid=FFE8717E7A93C957%21212&authkey=%21ACGzHPO2C5dfUHw&width=1366&height=768"
-                        titulo="Proyectos WinForms"
-                        descripcion={"Proyecto Windows Forms para la gestión de Comprobantes Electrónicos"} />
+                    
+                <Card
+                        url_imagen="https://lh3.googleusercontent.com/pw/AP1GczMa8sXrKukRQBjEtL54mjUnbi6xyJBpT_0uucMHjh9JgUVBV7l2jK64mlcHaXWbfbQiKV9b_ETVqY8kMzSjmtE_9aj_kOq4Us9Wh7MIQMqX93OPDmzNC30wo9Yrght9SSAoBVShPpvAEm4mQuJlFHg1=w1207-h651-s-no-gm"
+                        titulo="Proyectos de Escritorio WinForms"
+                        descripcion={"Proyecto para manejar XML de comprobantes electrónicos"} />
 
+                   
                     <Card
                         url_imagen="https://onedrive.live.com/embed?resid=FFE8717E7A93C957%21210&authkey=%21ANmxrQnwYrhiMoI&width=660"
                         titulo="Extensión de Google Chrome"
                         descripcion={"Extensión de Google Chrome para la descarga de Documentos Electrónicos"} />
 
                     <Card
-                        url_imagen="https://www.ictacademy.in/pages/assets/base/img/content/stock/asp.jpg"
-                        titulo="ASP"
-                        descripcion={"Creación de APIs Rest con ASP.NET"} />
+                        url_imagen="https://lh3.googleusercontent.com/pw/AP1GczOQOmUjjq3J2_AoKwQFVmTOrmYeUfyHf49Htdo7Cq7AIHxzZjswnOpyLpmRXBbya03TEv-QcWgZpeLAi-iS4qgzscy8meyJzantPVQ4qQ1xrMkYDfi6uqSCMhAdusEQMpWnMBSXj8BVLlsOqrBTwE6X=w957-h508-s-no-gm"
+                        titulo="Desarrollo en ASP.NET CORE"
+                        descripcion={"Api Rest de Facturación con .NET CORE"} />
 
                     <Card
-                        url_imagen="https://www.qindel.com/wp-content/uploads/2023/04/spring-boot.jpeg"
-                        titulo="JAVA- Spring Boot"
-                        descripcion={"Creación de APIs Rest con Java Spring Boot"} />
+                        url_imagen="src\assets\UsuarioRolesSpringBoot.png"
+                        titulo="Desarrollo en JAVA- Spring Boot"
+                        descripcion={"Sistema de Usuarios, roles y permisos en Spring Boot"} />
 
                     <Card
                         url_imagen="https://onedrive.live.com/embed?resid=FFE8717E7A93C957%21211&authkey=%21AE2pQFm32Jq48Io&width=660"
